43cfc7aac9eba58d7dab22d0cfe7f93593dc0912
Caio <caiosobreira77@gmail.com>
2022-06-30 12:06:17 -0300

    add libraries

--- src/index.js
+++ src/index.js
@@ -8,12 +8,21 @@ import chalk from 'chalk'
 
 dotenv.config()
 const PORT= process.env.PORT
+
 const app = express()
 app.use(cors())
 app.use(express())
 app.use(express.json())
 
 
+app.post('/users', (req, res) => {
+
+
+})
+
+app.get('/users', (req, res) => {
+
+})
 app.listen(PORT, ()=> {
-    console.log(chalk.bold.blue(`Server Online, port: ${PORT} `))
+    console.log(chalk.bold.green(`Server Online, port: ${PORT} `))
 })
